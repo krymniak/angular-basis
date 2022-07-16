d51b0739f32c8465c47e11e617a6e90d4c217015
krymniak <krymnyak@gmail.com>
2022-07-16 13:49:33 +0300

    basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basis';
+	// title = 'Dynamic title'
+	// number = 42
+	// arr = [1, 2, 3]
+	// obj = { a: 1, b: { c: 2 } }
+	// inputValue = ''
+	// img = 'https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png'
+
+	// constructor() {
+	// setTimeout(() => {
+	// 	this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png'
+	// }, 5000)
+	// }
+
+	// onInput(event?: any) {
+	// 	console.log('Event')
+	// 	this.inputValue = event.target.value
+	// }
+	// onClick(){
+	// 	console.log('click')
+	// }
+	// title = 'Initial'
+	// onInput(event:any){
+	// 	this.title = event.target.value
+	// }
+
+	// backgroundToggle: any = false
+
+
+	// 	arr = [1, 1, 2, 3, 5, 8, 13]
+	// 	objs = [
+	// 		{title: 'Post1', author: 'Krymniak', comments: [
+	// 			{name: 'Vitala', text:'lorem1'},
+	// 			{name: 'Vitala', text:'lorem1'},
+	// 			{name: 'Vitala', text:'lorem1'}
+	// 		]},
+	// 		{title: 'Post2', author: 'Krymniak', comments: [
+	// 			{name: 'Vitala2', text:'lorem2'},
+	// 			{name: 'Vitala2', text:'lorem2'},
+	// 			{name: 'Vitala2', text:'lorem2'}
+	// 		]}
+	// 	]
+	now: Date = new Date()
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 import { TestComponenComponent } from './test-componen/test-componen.component';
+import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponenComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		TestComponenComponent,
+		PostComponent,
+		Post2Component,
+		Post3Component
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
