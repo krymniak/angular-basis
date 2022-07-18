f450dd687f80209927bdf89bf6c5c56abc71c062
krymniak <krymnyak@gmail.com>
2022-07-18 10:46:00 +0300

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { TestComponenComponent } from './test-componen/test-componen.component';
-import { Post2Component } from './post2/post2.component';
-import { Post3Component } from './post3/post3.component';
 import { FormsModule } from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		TestComponenComponent,
 		PostComponent,
-		Post2Component,
-		Post3Component
+		PostFormComponent,
 	],
 	imports: [
 		BrowserModule,
