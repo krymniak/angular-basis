574b6020e84c21099910efa2e3ecb8538851df0c
krymniak <krymnyak@gmail.com>
2022-07-18 12:47:53 +0300

    access to HTML elements

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Post } from '../app.component';
 
 @Component({
@@ -9,7 +9,7 @@ import { Post } from '../app.component';
 export class PostFormComponent implements OnInit {
 
 	@Output() onAdd: EventEmitter<Post> = new EventEmitter()
-
+	@ViewChild('titleInput', { static: false }) inputRef!: ElementRef
 	title = ''
 	text = ''
 	constructor() { }
@@ -26,5 +26,8 @@ export class PostFormComponent implements OnInit {
 			console.log('New Post', post)
 			this.title = this.text = ''
 		}
+	}
+	focusTitle() {
+		this.inputRef.nativeElement.focus()
 	}
 }
