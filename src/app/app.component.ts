751f5a3604027f402338362501347667d386f1ae
krymniak <krymnyak@gmail.com>
2022-07-18 11:15:31 +0300

    transfer of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+export interface Post {
+	title: string
+	text: string
+	id?: number
+}
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+	posts: Post[] = [
+		{title: 'Want to learn Angular components', text:'I am still learning', id:1},
+		{title: 'Next block', text:'Will be about derectives and pipes', id:2}
+	]
 }
