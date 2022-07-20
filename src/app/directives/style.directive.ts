ea523b1705e9bd54f1536fab66f902770a540fe7
krymniak <krymnyak@gmail.com>
2022-07-20 13:01:22 +0300

    pass parameters to directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,11 +1,13 @@
 
-import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
 
 @Directive({
 	selector: '[appStyle]'
 })
 
 export class StyleDirective {
+@Input('appStyle') color: string = 'blue'
+@Input() fontWeight = 'normal'
 
 	constructor(private el:ElementRef, private r:Renderer2) {
 		
@@ -15,9 +17,11 @@ export class StyleDirective {
 
 	}
 	@HostListener('mouseenter') onEnter() {
-		this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+		this.r.setStyle(this.el.nativeElement, 'color', this.color)
+		this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
 	}
 	@HostListener('mouseleave') onLeave() {
 		this.r.setStyle(this.el.nativeElement, 'color', null)
+		this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
 	}
 }
