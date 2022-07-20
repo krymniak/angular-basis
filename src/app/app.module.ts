1f513a373d580f3eaf14b2c31fad7d7bf35c4b84
krymniak <krymnyak@gmail.com>
2022-07-20 12:32:46 +0300

    creating directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { StyleDirective } from './directives/style.directive';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		StyleDirective
 	],
 	imports: [
 		BrowserModule,
