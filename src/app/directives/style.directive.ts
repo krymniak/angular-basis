ba70283e507a2dd16c6ba6d1400b1ad0a7efd2da
krymniak <krymnyak@gmail.com>
2022-07-20 13:43:18 +0300

    HostBinding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,5 +1,5 @@
 
-import { Directive, ElementRef, HostListener, Input, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer2 } from "@angular/core";
 
 @Directive({
 	selector: '[appStyle]'
@@ -9,6 +9,7 @@ export class StyleDirective {
 @Input('appStyle') color: string = 'blue'
 @Input() fontWeight = 'normal'
 
+	@HostBinding('style.color') elColor:any = null
 	constructor(private el:ElementRef, private r:Renderer2) {
 		
 
@@ -17,11 +18,11 @@ export class StyleDirective {
 
 	}
 	@HostListener('mouseenter') onEnter() {
-		this.r.setStyle(this.el.nativeElement, 'color', this.color)
-		this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
+		this.elColor = this.color
+		//this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
 	}
 	@HostListener('mouseleave') onLeave() {
-		this.r.setStyle(this.el.nativeElement, 'color', null)
-		this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
+		this.elColor = null
+		//this.r.setStyle(this.el.nativeElement, 'fontWeight', null)
 	}
 }
