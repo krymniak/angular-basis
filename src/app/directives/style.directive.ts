c7c83ea347e9dde6c0dfcda4bfa9d07a3268b55a
krymniak <krymnyak@gmail.com>
2022-07-20 12:50:15 +0300

    adding events

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,5 +1,5 @@
 
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
 	selector: '[appStyle]'
@@ -8,7 +8,16 @@ import { Directive, ElementRef, Renderer2 } from "@angular/core";
 export class StyleDirective {
 
 	constructor(private el:ElementRef, private r:Renderer2) {
-		this.r.setStyle(el.nativeElement, 'color', 'blue')
+		
 
+	}
+	@HostListener('click', ['$event']) onClick(event: Event) {
+
+	}
+	@HostListener('mouseenter') onEnter() {
+		this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+	}
+	@HostListener('mouseleave') onLeave() {
+		this.r.setStyle(this.el.nativeElement, 'color', null)
 	}
 }
