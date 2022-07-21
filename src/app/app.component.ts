817ce06d892aa2a0473b86ec3aed7a7c039cf66e
krymniak <krymnyak@gmail.com>
2022-07-21 12:22:58 +0300

    other pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ export class AppComponent {
 	e: number = Math.E
 	str: string = 'hello world'
 	date: Date = new Date()
+	float = 0.42
+	obj = {
+		a:1,
+		b:{
+			c:2,
+			d:{
+				e:3,
+				f:4
+			}
+		}
+	}
 }
