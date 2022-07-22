4b5989e4827505cfbd8e232db57a0c3d9b73e170
krymniak <krymnyak@gmail.com>
2022-07-22 13:57:01 +0300

    cleared project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { AppCounterService } from './services/app-counter.service';
-import { LocalCounterService } from './services/local-counter.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
-	providers: [LocalCounterService]
 })
 export class AppComponent {
-	constructor(public appCounterServise: AppCounterService,
-		public localCounterServise: LocalCounterService ) {
 
-	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { AppCounterService } from './services/app-counter.service';
-import { CounterComponent } from './counter/counter.component';
+
 
 
 
 @NgModule({
 	declarations: [
-		AppComponent,
-		CounterComponent,
+		AppComponent
 	],
 	imports: [
 		BrowserModule,
@@ -20,7 +18,6 @@ import { CounterComponent } from './counter/counter.component';
 		FormsModule
 	],
 	providers: [
-		AppCounterService
 	],
 	bootstrap: [AppComponent]
 })
