ecbb33007cf0e27a3a425d292a7382f7e545cc01
krymniak <krymnyak@gmail.com>
2022-07-22 12:46:56 +0300

    creating services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AppCounterService } from './services/app-counter.service';
 
 
 
@@ -16,7 +17,9 @@ import { FormsModule } from '@angular/forms';
 		AppRoutingModule,
 		FormsModule
 	],
-	providers: [],
+	providers: [
+		AppCounterService
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/app-counter.service.ts
@@ -0,0 +1,10 @@
+export class AppCounterService {
+	counter = 0
+	increase() {
+		this.counter++
+	}
+
+	decrease() {
+		this.counter--
+	}
+}
