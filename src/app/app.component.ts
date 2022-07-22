079257ecc1ce298873a07b71726c3285396a11c9
krymniak <krymnyak@gmail.com>
2022-07-22 12:16:07 +0300

    cleared project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
 
-export interface Post {
-	title: string
-	text: string
-}
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	p: Promise<string> = new Promise<string>(resolve => {
-		setTimeout(()=> {
-			resolve('Promise Resolved')
-		}, 5000)
-	})
 
-	date: Observable<Date> = new Observable(obs=>{
-		setInterval(() => {
-			obs.next(new Date())
-		}, 1000)
-	})
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { MultByPipe } from './pipes/mult-by.pipe';
-import { ExMarksPipe } from './pipes/ex-marks.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
 
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		MultByPipe,
-  ExMarksPipe,
-  FilterPipe
 	],
 	imports: [
 		BrowserModule,
