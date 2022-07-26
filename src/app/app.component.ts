aae09252233a468c3cfa0eab09dc2c62b8d61605
krymniak <krymnyak@gmail.com>
2022-07-26 12:45:38 +0300

    rxjs subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { interval, Subscription, Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators'
+import { Subscription, Subject } from 'rxjs';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -8,32 +7,18 @@ import { map, filter } from 'rxjs/operators'
 })
 export class AppComponent {
 	sub: Subscription
+	stream$: Subject<number> = new Subject<number>()
+	counter = 0
 	constructor() {
-		const stream$ = new Observable(observer => {
-			setTimeout(() => {
-				observer.next(1)
-			}, 1500)
-
-			setTimeout(() => {
-				observer.complete()
-			}, 1900)
-
-			setTimeout(() => {
-				observer.error('Something went wrong')
-			}, 2000)
-
-			setTimeout(() => {
-				observer.next(2)
-			}, 2500)
+		this.sub = this.stream$.subscribe(value => {
+			console.log('Subscribe', value)
 		})
-		this.sub = stream$
-			.subscribe(
-				value => console.log(value),
-				error => console.log(error),
-				() => console.log('Complete')
-			)
 	}
 	stop() {
-		this.sub.unsubscribe
+		this.sub.unsubscribe()
+	}
+	next() {
+		this.counter++
+		this.stream$.next(this.counter)
 	}
 }
