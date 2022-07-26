250ece643980eaef4667430b345e4fa31d2adf3f
krymniak <krymnyak@gmail.com>
2022-07-26 12:50:42 +0300

    cleared project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,13 @@
-import { Component } from '@angular/core';
-import { Subscription, Subject } from 'rxjs';
+import {Component, OnInit} from '@angular/core'
+
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-	sub: Subscription
-	stream$: Subject<number> = new Subject<number>()
-	counter = 0
-	constructor() {
-		this.sub = this.stream$.subscribe(value => {
-			console.log('Subscribe', value)
-		})
-	}
-	stop() {
-		this.sub.unsubscribe()
-	}
-	next() {
-		this.counter++
-		this.stream$.next(this.counter)
-	}
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+
+  }
 }
