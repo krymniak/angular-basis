f2ad9d0bbf8960e43123ee13f90db61d9ac907ae
krymniak <krymnyak@gmail.com>
2022-07-26 13:27:26 +0300

    post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,42 @@
 import { HttpClient } from '@angular/common/http';
-import {Component, OnInit} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 export interface Todo {
 	completed: boolean
 	title: string
 	id?: number
 }
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
 	todos: Todo[] = []
+	todoTitle = ''
 
-	constructor(private http:HttpClient) {
+	constructor(private http: HttpClient) {
 
 	}
-  ngOnInit() {
+	ngOnInit() {
 		this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-		.subscribe(response => {
-			console.log(response)
-			this.todos = response
-		})
-  }
+			.subscribe(response => {
+				console.log(response)
+				this.todos = response
+			})
+	}
+	addTodo() {
+		if (!this.todoTitle.trim()) {
+			return
+		}
+		const newTodo: Todo = {
+			title: this.todoTitle,
+			completed: false
+		}
+		this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+			.subscribe(todo => {
+				this.todos.push(todo)
+				this.todoTitle = ''
+			})
+	}
 }
