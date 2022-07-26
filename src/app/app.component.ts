43f27d4d7cd2a1776c697f429f5272509942b202
krymniak <krymnyak@gmail.com>
2022-07-26 12:18:24 +0300

    creating stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component} from '@angular/core';
-import { interval, Subscription } from 'rxjs';
-import {map, filter} from 'rxjs/operators'
+import { Component } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators'
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -9,18 +9,29 @@ import {map, filter} from 'rxjs/operators'
 export class AppComponent {
 	sub: Subscription
 	constructor() {
-		const intervalStream$ = interval(1000)
+		const stream$ = new Observable(observer => {
+			setTimeout(() => {
+				observer.next(1)
+			}, 1500)
 
-		this.sub = intervalStream$
-		.pipe(
-			filter(value => value % 2 === 0),
-			map((value) => {
-				`Mapped value ${value}`
-			})
-		)
-		.subscribe((value) => {
+			setTimeout(() => {
+				observer.complete()
+			}, 1900)
 
+			setTimeout(() => {
+				observer.error('Something went wrong')
+			}, 2000)
+
+			setTimeout(() => {
+				observer.next(2)
+			}, 2500)
 		})
+		this.sub = stream$
+			.subscribe(
+				value => console.log(value),
+				error => console.log(error),
+				() => console.log('Complete')
+			)
 	}
 	stop() {
 		this.sub.unsubscribe
