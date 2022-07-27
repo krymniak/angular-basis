0965d6e04d5581ec401b5d56269d3376167d9400
krymniak <krymnyak@gmail.com>
2022-07-27 12:10:55 +0300

    params

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, delay, Observable, throwError } from "rxjs";
 
@@ -24,7 +24,13 @@ export class TodosService {
 
 	}
 	fetchTodos(): Observable<Todo[]> {
-		return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos9?_limit=2')
+		let params = new HttpParams()
+		params = params.append('_limit', '4')
+		params = params.append('custom', 'anything')
+		return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?', {
+			//params: new HttpParams().set('_limit', '3')
+			params
+		})
 			.pipe(
 				delay(500),
 				catchError(error => {
