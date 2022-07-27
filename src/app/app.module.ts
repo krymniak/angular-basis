317889b380e58f277080c3f0096b8005810871d9
krymniak <krymnyak@gmail.com>
2022-07-27 14:22:19 +0300

    cleared project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
 import {BrowserModule} from '@angular/platform-browser'
-import {NgModule, Provider} from '@angular/core'
+import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
+import {FormsModule} from '@angular/forms'
+import {AboutComponent} from './about/about.component'
+import {HomeComponent} from './home/home.component'
+import {PostsComponent} from './posts/posts.component'
+import {PostComponent} from './post/post.component'
+import {AboutExtraComponent} from './about-extra/about-extra.component'
 
-const INTERCEPTOR_PROVIDER: Provider = {
-	provide: HTTP_INTERCEPTORS,
-	useClass: AuthInterceptor,
-	multi: true
-}
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
+    HomeComponent,
+    PostsComponent,
+    PostComponent,
+    AboutExtraComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-		HttpClientModule
   ],
-  providers: [INTERCEPTOR_PROVIDER],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
-
