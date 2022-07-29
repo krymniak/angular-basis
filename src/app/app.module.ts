939f8e4e31e01286d139678c082e5358563d2455
krymniak <krymnyak@gmail.com>
2022-07-29 13:23:40 +0300

    Dynamic components 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms'
 import { ModalComponent } from './modal/modal.component'
+import { RefDirective } from './ref.directive'
 
 @NgModule({
 	declarations: [
 		AppComponent,
-  ModalComponent
+  ModalComponent,
+	RefDirective
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
 	],
 	providers: [],
+	entryComponents: [ModalComponent],
 	bootstrap: [AppComponent]
 })
 export class AppModule {
