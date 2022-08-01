c3f23b83652f7feab90d9963f8458b60f6583f8b
krymniak <krymnyak@gmail.com>
2022-08-01 12:17:34 +0300

    form test

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,11 @@
+import { FormBuilder } from "@angular/forms"
 import { CounterComponent } from "./counter.component"
 
 describe('CounterComponent', () => {
 	let component: CounterComponent
 
 	beforeEach(() => {
-		component = new CounterComponent
+		component = new CounterComponent(new FormBuilder())
 	})
 	it('should increment counter by 1', () => {
 		component.increment()
@@ -17,9 +18,22 @@ describe('CounterComponent', () => {
 	})
 
 	it('should increment value by event emitter', () => {
-		let result = null
+		let result = 0
 		component.counterEmitter.subscribe(v => result = v)
 		component.increment()
 		expect(result).toBe(1)
+	})
+
+	it('should create form with 2 controls', () => {
+		expect(component.form.contains('login')).toBeTruthy()
+		expect(component.form.contains('email')).toBeTruthy()
+	})
+
+	it('should mark login as invalid if empty value', () => {
+		const control = component.form.get('login')
+
+		control?.setValue('')
+
+		expect(control?.valid).toBeFalsy
 	})
 })
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
 	selector: 'app-counter',
@@ -7,6 +8,13 @@ import { Component, EventEmitter, Output } from "@angular/core";
 
 export class CounterComponent {
 	counter = 0
+	public form: FormGroup
+	constructor(private fb: FormBuilder) {
+		this.form = fb.group({
+			login: ['', Validators.required],
+			email: ['']
+		})
+	}
 
 	@Output() counterEmitter = new EventEmitter<number>()
 
