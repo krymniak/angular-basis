862bd89cb6ebf45384b0ef272c85848b834e6def
krymniak <krymnyak@gmail.com>
2022-08-01 10:58:01 +0300

    PWA module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { FormsModule } from '@angular/forms'
 import { ModalComponent } from './modal/modal.component'
-import { RefDirective } from './ref.directive'
+import { RefDirective } from './ref.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [
@@ -15,6 +17,12 @@ import { RefDirective } from './ref.directive'
 	imports: [
 		BrowserModule,
 		FormsModule,
+  ServiceWorkerModule.register('ngsw-worker.js', {
+    enabled: environment.production,
+    // Register the ServiceWorker as soon as the application is stable
+    // or after 30 seconds (whichever comes first).
+    registrationStrategy: 'registerWhenStable:30000'
+  }),
 	],
 	providers: [],
 	entryComponents: [ModalComponent],
