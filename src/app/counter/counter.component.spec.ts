b903f4191d3313647833898360cf4e0184014358
krymniak <krymnyak@gmail.com>
2022-08-02 11:08:53 +0300

    setting

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,10 +1,21 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 import {CounterComponent} from "./counter.component";
 
 describe('CounterComponent', () => {
   let component: CounterComponent
+	let fixture: ComponentFixture<CounterComponent>
 
   beforeEach(() => {
-    component = new CounterComponent()
+		TestBed.configureTestingModule({
+			declarations:[CounterComponent]
+		})
+
+		fixture = TestBed.createComponent(CounterComponent)
+		component = fixture.componentInstance
   })
+
+	it('should be created', () => {
+		expect(component).toBeDefined()
+	})
 
 })
