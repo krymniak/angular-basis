06961d471caf6e653d0ec8b53a15b46fb0f7c5df
krymniak <krymnyak@gmail.com>
2022-08-03 12:37:16 +0300

    keyframes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, group, query, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, query, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -31,8 +31,14 @@ import { Component } from '@angular/core';
 				animate(750)]),
 			]),
 			transition(':enter', [
-				style({ opacity: 0}),
-				animate('800ms ease-out')
+				animate('4s', keyframes([
+					style({background: 'red', offset: 0}),
+					style({background: 'black', offset: 0.2}),
+					style({background: 'orange', offset: 0.3}),
+					style({background: 'blue', offset: 1}),
+				]))
+				// style({ opacity: 0}),
+				// animate('800ms ease-out')
 			]),
 			transition(':leave', [
 				style({opacity: 1}),
