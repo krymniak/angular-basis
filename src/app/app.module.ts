ca9f76175cf46c548a34425a5c5e316929f7203a
krymniak <krymnyak@gmail.com>
2022-08-03 13:16:42 +0300

    ng animate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {FormsModule} from "@angular/forms";
+import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
 import { RouterModule } from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnimateComponent } from './animate/animate.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
+	declarations: [
+		AppComponent,
+		AnimateComponent,
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
 		RouterModule,
 		BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
